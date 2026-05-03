4c7e8ea8bf4ff9ecc81cf601d86d284daea5dc50
earnity-omchain <earnitytempo@gmail.com>
2026-05-03 09:22:05 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,6 @@ import Leaderboard from "@/pages/leaderboard";
 import Connect from "@/pages/connect";
 import AuthCallback from "@/pages/auth-callback";
 import Profile from "@/pages/profile";
-import GuildWaiting from "@/pages/guild-waiting";
 import NotFound from "@/pages/not-found";
 import { useEffect } from "react";
 
@@ -84,7 +83,6 @@ function AppRouter() {
       {/* Protected routes */}
       <Route path="/dashboard" component={() => <ProtectedRoute component={Dashboard} />} />
       <Route path="/profile" component={() => <ProtectedRoute component={Profile} />} />
-      <Route path="/guild-waiting" component={() => <ProtectedRoute component={GuildWaiting} />} />
 
       <Route component={NotFound} />
     </Switch>
